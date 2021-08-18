b8325e1f03836fdf5139ef7419f804bb75288c2c
朱骏杰 <zhujunjie@sundear.com>
2021-08-18 17:26:14 +0800

    test ci update

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+var webpackConfig = require('@vue/cli-service/webpack.config.js')
+
+console.log('fuck', webpackConfig)
+
+const path = require('path')
+
+module.exports = function (config) {
+  config.set({
+    frameworks: ['mocha'],
+
+    files: [
+      'tests/**/*.spec.js'
+    ],
+
+    preprocessors: {
+      '**/*.spec.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: webpackConfig,
+
+    reporters: ['spec'],
+
+    browsers: ['Chrome']
+  })
+}
